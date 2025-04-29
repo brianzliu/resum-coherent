179c577f904a19198c59ed86a7fda8e5cd031363
brianzliu <brianliu0317@gmail.com>
2025-04-29 12:51:33 -0700

    idk

--- resum/utilities/utilities.py
+++ resum/utilities/utilities.py
@@ -71,88 +71,90 @@ def get_dataframes_concat(path, nrows=None):
     return df_new, len(file_list)
 
 def convert_single_csv_to_hdf5(csv_file, hdf5_file, theta_headers, target_label, weights_labels):
-        """
-        """
-        # Read CSV file
-        df = pd.read_csv(csv_file)
-        # added strip
-        df.columns = df.columns.str.strip()
-        # **Extract 'theta' data**
-        theta_data = df[theta_headers].to_numpy()[0]
-        #weights_data = df[weights_labels].to_numpy()
-        target_data = df[target_label].to_numpy()
-        # **Extract 'phi' (all columns except theta_headers, 'target', 'weights')**
-        phi_headers = [col for col in df.columns if col not in theta_headers + target_label + weights_labels + ['fidelity']]
-        #phi_headers = [col for col in df.columns if col not in theta_headers + target_label + weights_labels ]
-        
-        phi_data = df[phi_headers].to_numpy()
+    """
+    """
+    # Read CSV file
+    df = pd.read_csv(csv_file)
+    # added strip
+    df.columns = df.columns.str.strip()
 
-        # Filter out non-existing columns from weights
-        existing_weights = [w for w in weights_labels if w in df.columns]
-        # Select only available columns, or default to ones
-        weights_data = df[existing_weights].to_numpy() if existing_weights else np.ones((len(df), 1))
-        weights_labels = existing_weights if existing_weights else ["weights"]
-        fidelity_data = df[['fidelity']].to_numpy() if 'fidelity' in df else np.ones((len(df), 1))
+    # **Extract 'theta' data**
+    theta_data = df[theta_headers].to_numpy()[0]
+    #weights_data = df[weights_labels].to_numpy()
+    target_data = df[target_label].to_numpy()
+    # **Extract 'phi' (all columns except theta_headers, 'target', 'weights')**
+    phi_headers = [col for col in df.columns if col not in theta_headers + target_label + weights_labels + ['fidelity']]
+    #phi_headers = [col for col in df.columns if col not in theta_headers + target_label + weights_labels ]
+    
+    phi_data = df[phi_headers].to_numpy()
 
-        # **Store Data in HDF5**
-        with h5py.File(hdf5_file, "w") as hdf:
-            hdf.create_dataset("fidelity", data=fidelity_data, compression="gzip")
-            hdf.create_dataset("theta", data=theta_data, compression="gzip")
-            hdf.create_dataset("theta_headers", data=np.array(theta_headers, dtype='S'), compression="gzip")
-            hdf.create_dataset("phi", data=phi_data, compression="gzip")
-            hdf.create_dataset("phi_labels", data=np.array(phi_headers, dtype='S'), compression="gzip")
-            hdf.create_dataset("target", data=target_data, compression="gzip")
-            hdf.create_dataset("target_labels", data=np.array(target_label, dtype='S'), compression="gzip")
-            hdf.create_dataset("weights", data=weights_data, compression="gzip")
-            hdf.create_dataset("weights_labels", data=np.array(weights_labels, dtype='S'), compression="gzip")
+    # Filter out non-existing columns from weights
+    existing_weights = [w for w in weights_labels if w in df.columns]
+    # Select only available columns, or default to ones
+    weights_data = df[existing_weights].to_numpy() if existing_weights else np.ones((len(df), 1))
+    weights_labels = existing_weights if existing_weights else ["weights"]
+    fidelity_data = df[['fidelity']].to_numpy() if 'fidelity' in df else np.ones((len(df), 1))
+
+    # **Store Data in HDF5**
+    with h5py.File(hdf5_file, "w") as hdf:
+        hdf.create_dataset("fidelity", data=fidelity_data, compression="gzip")
+        hdf.create_dataset("theta", data=theta_data, compression="gzip")
+        hdf.create_dataset("theta_headers", data=np.array(theta_headers, dtype='S'), compression="gzip")
+        hdf.create_dataset("phi", data=phi_data, compression="gzip")
+        hdf.create_dataset("phi_labels", data=np.array(phi_headers, dtype='S'), compression="gzip")
+        hdf.create_dataset("target", data=target_data, compression="gzip")
+        hdf.create_dataset("target_labels", data=np.array(target_label, dtype='S'), compression="gzip")
+        hdf.create_dataset("weights", data=weights_data, compression="gzip")
+        hdf.create_dataset("weights_labels", data=np.array(weights_labels, dtype='S'), compression="gzip")
+        hdf.create_dataset("target_headers", data=np.array(target_label, dtype='S'), compression="gzip")
 
 
 def convert_all_csv_to_hdf5(config_file):
-        """
-        Loops over all CSV files in `csv_dir` and converts each to an HDF5 file.
-        
-        Parameters:
-        - csv_dir (str): Directory containing CSV files.
-        - hdf5_dir (str): Directory where HDF5 files will be saved.
-        - theta_headers (list of str): Column names that should be stored under 'theta'.
-        
-        Each CSV file is stored as an individual HDF5 file in `hdf5_dir`.
-        """
-        path_to_files=config_file["path_settings"]["path_to_test_files_lf"]
+    """
+    Loops over all CSV files in `csv_dir` and converts each to an HDF5 file.
+    
+    Parameters:
+    - csv_dir (str): Directory containing CSV files.
+    - hdf5_dir (str): Directory where HDF5 files will be saved.
+    - theta_headers (list of str): Column names that should be stored under 'theta'.
+    
+    Each CSV file is stored as an individual HDF5 file in `hdf5_dir`.
+    """
+    path_to_files=config_file["path_settings"]["path_to_test_files_lf"]
 
-        theta_headers= config_file["simulation_settings"]["theta_headers"]
-        target_label = config_file["simulation_settings"]["target_label"]
-        weights_labels = config_file["simulation_settings"]["weights_labels"]
+    theta_headers= config_file["simulation_settings"]["theta_headers"]
+    target_label = config_file["simulation_settings"]["target_label"]
+    weights_labels = config_file["simulation_settings"]["weights_labels"]
 
-        # Loop through all CSV files
-        print("Converting CSV to HDF5")
-        for file in tqdm(sorted(os.listdir(path_to_files))):
-            if file.endswith(".csv"):
-                csv_file = os.path.join(path_to_files, file)
-                hdf5_file = os.path.join(path_to_files, file.replace(".csv", ".h5"))  # Save as .hdf5
-                # Call the function for single file conversion
-                convert_single_csv_to_hdf5(csv_file, hdf5_file, theta_headers, target_label, weights_labels)
+    # Loop through all CSV files
+    print("Converting CSV to HDF5")
+    for file in tqdm(sorted(os.listdir(path_to_files))):
+        if file.endswith(".csv"):
+            csv_file = os.path.join(path_to_files, file)
+            hdf5_file = os.path.join(path_to_files, file.replace(".csv", ".h5"))  # Save as .hdf5
+            # Call the function for single file conversion
+            convert_single_csv_to_hdf5(csv_file, hdf5_file, theta_headers, target_label, weights_labels)
 
 def read_selected_indices(hdf5_file, label_dict):
-        """
-        Gives back indices of only the specified columns from parameter_key inHDF5 file.
-        label_dict is a dictionary with following structure {'key': "...",'label_key': "....",'selected_labels': ["radius","thickness",...]}
-        """
-        with h5py.File(hdf5_file, "r") as hdf:
-            if label_dict['label_key'] not in hdf:
-                selected_indices= [0] if len(hdf[label_dict['key']].shape) == 1 else list(range(hdf[label_dict['key']].shape[1]))
-            else:
-                selected_labels=label_dict['selected_labels']
-                if selected_labels == None: 
-                    selected_labels=hdf[label_dict['label_key']][:]
+    """
+    Gives back indices of only the specified columns from parameter_key inHDF5 file.
+    label_dict is a dictionary with following structure {'key': "...",'label_key': "....",'selected_labels': ["radius","thickness",...]}
+    """
+    with h5py.File(hdf5_file, "r") as hdf:
+        if label_dict['label_key'] not in hdf:
+            selected_indices= [0] if len(hdf[label_dict['key']].shape) == 1 else list(range(hdf[label_dict['key']].shape[1]))
+        else:
+            selected_labels=label_dict['selected_labels']
+            if selected_labels == None: 
+                selected_labels=hdf[label_dict['label_key']][:]
 
-                labels = list(map(lambda x: x.decode() if isinstance(x, bytes) else x, hdf[label_dict['label_key']][:]))
-                # Find indices of required columns **before** reading phi
-                selected_indices = [labels.index(label) for label in selected_labels if label in labels]
+            labels = list(map(lambda x: x.decode() if isinstance(x, bytes) else x, hdf[label_dict['label_key']][:]))
+            # Find indices of required columns **before** reading phi
+            selected_indices = [labels.index(label) for label in selected_labels if label in labels]
 
-            if not selected_indices:
-                raise ValueError(f"None of the requested labels {selected_labels} exist in {label_dict['key']}!")
-            return sorted(selected_indices)
+        if not selected_indices:
+            raise ValueError(f"None of the requested labels {selected_labels} exist in {label_dict['key']}!")
+        return sorted(selected_indices)
 
 def get_all_signal_events(filename_base, nrows):
     # Set parameter name/x_labels -> needs to be consistent with data input file
